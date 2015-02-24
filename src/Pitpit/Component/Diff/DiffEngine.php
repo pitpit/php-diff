eb0f91a224d6570e5b480baf1c164cea37d948a1
Damien <d.pitard@creads.org>
2015-02-24 10:07:42 +0100

    added exclude pattern in DiffEngine

--- src/Pitpit/Component/Diff/DiffEngine.php
+++ src/Pitpit/Component/Diff/DiffEngine.php
@@ -10,16 +10,19 @@ namespace Pitpit\Component\Diff;
 class DiffEngine
 {
     protected $filter;
+    protected $excludes;
 
     /**
      * Set methods or properties you want to compare in a classe
      *
      * @param ReflectionProperty constants (see http://php.net/manual/en/class.reflectionproperty.php)
+     * @param array Mapping of class and properties to exclude
      *
      */
-    public function __construct($filter = null)
+    public function __construct($filter = null, $excludes = array())
     {
         $this->filter = $filter;
+        $this->excludes = $excludes;
     }
 
     /**
@@ -56,34 +59,39 @@ class DiffEngine
                 //throw new \InvalidArgumentException(sprintf('Unable to compare objects of different classes, identifier: %s', $identifier?$identifier:'null'));
             }
 
+
+
             $reflectionOld = !is_null($old)?new \ReflectionClass($old):null;
             $reflectionNew = !is_null($new)?new \ReflectionClass($new):null;
 
-            $map = array();
+            $map = array_merge($this->buildMap($reflectionOld), $this->buildMap($reflectionNew));
+            // $map = array();
 
-            if (!is_null($reflectionOld)) {
-                $map[get_class($old)] = array();
-                if ($this->filter) {
-                    $properties = $reflectionOld->getProperties($this->filter);
-                } else {
-                    $properties = $reflectionOld->getProperties();
-                }
-                foreach ($properties as $property) {
-                    $map[get_class($old)][] = $property->getName();
-                }
-            }
+            // if (!is_null($reflectionOld)) {
+            //     $map[get_class($old)] = array();
+            //     if ($this->filter) {
+            //         $properties = $reflectionOld->getProperties($this->filter);
+            //     } else {
+            //         $properties = $reflectionOld->getProperties();
+            //     }
+            //     foreach ($properties as $property) {
+            //         if (!isset($this->exclude[get_class($old)]) || !in_array($property->getName(), $this->exclude[get_class($old)])) {
+            //             $map[get_class($old)][] = $property->getName();
+            //         }
+            //     }
+            // }
 
-            if (!is_null($reflectionNew)) {
-                $map[get_class($new)] = array();
-                if ($this->filter) {
-                    $properties = $reflectionNew->getProperties($this->filter);
-                } else {
-                    $properties = $reflectionNew->getProperties();
-                }
-                foreach ($properties as $property) {
-                    $map[get_class($new)][] = $property->getName();
-                }
-            }
+            // if (!is_null($reflectionNew)) {
+            //     $map[get_class($new)] = array();
+            //     if ($this->filter) {
+            //         $properties = $reflectionNew->getProperties($this->filter);
+            //     } else {
+            //         $properties = $reflectionNew->getProperties();
+            //     }
+            //     foreach ($properties as $property) {
+            //         $map[get_class($new)][] = $property->getName();
+            //     }
+            // }
 
             $done = array();
             if (!is_null($reflectionNew) && isset($map[get_class($new)])) {
@@ -171,5 +179,34 @@ class DiffEngine
         $diff->setStatus($status);
 
         return $diff;
+    }
+
+
+    /**
+     * Build a map of properties to scan for diffs by class
+     *
+     * @return array An array where keys are fully classified class names and value arrays of properties
+     */
+    protected function buildMap(\ReflectionClass $reflection = null)
+    {
+        $map = array();
+        if (!is_null($reflection)) {
+            $class = $reflection->getName();
+
+            $map[$class] = array();
+            if ($this->filter) {
+                $properties = $reflection->getProperties($this->filter);
+            } else {
+                $properties = $reflection->getProperties();
+            }
+
+            foreach ($properties as $property) {
+                if (!isset($this->excludes[$class]) || !in_array($property->getName(), $this->excludes[$class])) {
+                    $map[$class][] = $property->getName();
+                }
+            }
+        }
+
+        return $map;
     }
 }
